ade0ef71f11e6ebf65467d500bfb2f32fb3d615b
Kanishka46 <kanishkakrishnan46@gmail.com>
2026-02-26 15:21:24 +0530

    install setup

---
+++ smartcareerbackend/src/main/java/com/example/smartcareerbackend/SmartcareerbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartcareerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcareerbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcareerbackendApplication.class, args);
+	}
+
+}
---
+++ smartcareerbackend/src/test/java/com/example/smartcareerbackend/SmartcareerbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartcareerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcareerbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
